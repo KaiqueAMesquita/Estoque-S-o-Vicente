f1223ad75f83eb7d23d6e498c5a5b015057937cb
Gustavo <gustavinhoglp@gmail.com>
2025-03-30 23:01:53 -0300

    feat: Criação projeto backend

---
+++ backend/src/main/java/com/estocados/GerenciamentoDeEstoqueLSP/GerenciamentoDeEstoqueLspApplication.java
@@ -0,0 +1,13 @@
+package com.estocados.GerenciamentoDeEstoqueLSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeEstoqueLspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeEstoqueLspApplication.class, args);
+	}
+
+}
