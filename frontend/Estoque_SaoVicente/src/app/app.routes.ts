5fdf888d34d3aa6aa04b8f6f9b094831e737cb20
j <@gmail.com>
2025-04-20 22:32:07 -0300

    Created UsersTable Component

--- frontend/Estoque_SaoVicente/src/app/app.routes.ts
+++ frontend/Estoque_SaoVicente/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { UsersTableComponent } from './shared/components/users-table/users-table.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent, },
+    { path: 'users', component: UsersTableComponent, },
 ];
